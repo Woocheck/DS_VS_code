cedaf6b0407978683d42d2a8cbdb882c3fa661c2
Woocheck <34909593+Woocheck@users.noreply.github.com>
2021-12-20 11:16:35 +0100

    index 2

--- generation.py
+++ generation.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 df = pd.read_excel("PL_GEN_WIATR_20211103_20211202_20211203070532.xlsm",
-                   "PL_GEN_WIATR_20211103_20211202_")
+                   "PL_GEN_WIATR_20211103_20211202_", index_col='Data')
+
+df
 
 plt.figure()
 
