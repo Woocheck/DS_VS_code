b06ea58f244f96b9f6e56214afc0d3b04186ad22
Woocheck <34909593+Woocheck@users.noreply.github.com>
2021-12-20 08:59:24 +0100

    generation

---
+++ generation.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel("PL_GEN_WIATR_20211103_20211202_20211203070532.xlsm",
+                   "PL_GEN_WIATR_20211103_20211202_")
---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_excel("2_lista_plac.xlsx", "Lista personalna")
+print(df)
